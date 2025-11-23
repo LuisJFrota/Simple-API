cc46e84c7ea57e203d22998261b55e7b02c05516
Luis Frota <luisjuniorfrota@gmail.com>
2025-11-22 21:52:16 -0300

    consult

--- app.py
+++ app.py
@@ -7,12 +7,26 @@ books = [
         'id': 1,
         'title': 'The Strain',
         'author': 'Chuck Hogan'
+    },
+    {
+        'id': 2,
+        'title': 'It',
+        'author': 'Stephen King'
     }
 ]
 
 
-@app.route('/books')
+@app.route('/books', methods=['GET'])
 def listBooks():
     return jsonify(books)
+
+@app.route('/booksbyid/<int:id>', methods=['GET'])
+def listBookbyId(id):
+    for book in books:
+        if(book.get('id') == id):
+            return jsonify(book)
+    pass
+        
+    
 
 app.run(port=5000, host='localhost', debug=True)
